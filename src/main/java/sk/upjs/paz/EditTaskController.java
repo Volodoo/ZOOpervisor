b9c0c6c2d5f33bfa6797a3301685616a62aa9da8
MatusKacmar <5396375@upjs.sk>
2025-12-09 15:22:37 +0100

    dalse okno nove a upravy nejake zatial tak jak je

---
+++ src/main/java/sk/upjs/paz/EditTaskController.java
@@ -0,0 +1,15 @@
+package sk.upjs.paz;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class EditTaskController {
+
+    @FXML
+    public void saveEdit(ActionEvent event) {}
+
+
+    @FXML
+    public void goBack(ActionEvent event) {}
+
+}
---
+++ src/main/java/sk/upjs/paz/MainViewController.java
@@ -0,0 +1,26 @@
+package sk.upjs.paz;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainViewController {
+
+
+    @FXML
+    public void displayAnimals(ActionEvent event) {
+    }
+    @FXML
+    public void displayEnclosures(ActionEvent event) {
+    }
+    @FXML
+    public void displayTasks(ActionEvent event) {
+    }
+    @FXML
+    public void displayDashboard(ActionEvent event) {
+    }
+    @FXML
+    public void displayTicketSeller(ActionEvent event) {
+    }
+
+
+}
